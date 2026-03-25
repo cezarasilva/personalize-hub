b9aff1c388cee7ecabfa66d36f9bc93fb1128c3f
cezarasilva <cezar.antonio.silva@gmail.com>
2026-03-24 22:07:38 -0300

    Ajustando db.js para usar variavel de ambiente e SSL

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,19 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
-// Colocando o link direto com a sua senha real para testar
-const linkDireto = "postgresql://postgres:paWIMBVHTtmk0KAX@db.xdopczsuuawbxrfryaes.supabase.co:5432/postgres";
-
+// Agora ele vai puxar o link verde e seguro lá das configurações do Render (ou do seu .env local)
 const pool = new Pool({
-    connectionString: linkDireto
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Obrigatório para a nuvem não barrar a conexão
+    }
 });
 
 pool.connect((err, client, release) => {
     if (err) {
         return console.error('❌ Erro ao conectar no banco de dados:', err.stack);
     }
-    console.log('✅ Banco de dados conectado com sucesso, Rogério!');
+    console.log('✅ Banco de dados conectado com sucesso na Nuvem!');
     release();
 });
 
